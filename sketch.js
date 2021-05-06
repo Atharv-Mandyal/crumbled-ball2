622953fe6cdfa045c7ca33a90f26db7c09196e12
Atharv-Mandyal <80249738+Atharv-Mandyal@users.noreply.github.com>
2021-05-06 16:52:02 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload() {
+  backgroundImg = loadImage("bg.jpg");
+}
+
+function setup() {
+var canvas =	createCanvas(1000, 400);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	ground = new Ground(400,height,1200,20)
+	ball = new Ball(100,200,20,20)
+	dustbinline1 = new Dustbin(865,350,10,90)
+	dustbinline2= new Dustbin (755,350,10,90)
+	dustbinline3= new Dustbin (810,380,100,20)
+
+	
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(backgroundImg);
+  ground.display();
+  ball.display();
+  dustbinline1.display();
+  dustbinline2.display();
+  dustbinline3.display();
+  
+  drawSprites();
+ 
+}
+
+
+
